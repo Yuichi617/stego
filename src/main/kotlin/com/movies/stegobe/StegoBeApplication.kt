85e4def49487386919d0c7c1e69d62a2a9f807ea
Yuichi Sugiyama <ozwald176@gmail.com>
2022-11-06 12:29:44 +0900

    setup

---
+++ src/main/kotlin/com/movies/stegobe/StegoBeApplication.kt
@@ -0,0 +1,11 @@
+package com.movies.stegobe
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StegoBeApplication
+
+fun main(args: Array<String>) {
+	runApplication<StegoBeApplication>(*args)
+}
